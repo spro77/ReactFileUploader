db41a62292af08d08c9f97fe4808c48b8f95ac11
Sergey Prostov <spro77@gmail.com>
2023-08-08 21:04:35 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import FileUploader from "./FileUploader";
 
+const Colors = {
+  MainText: '#333',
+  Primary: '#3498db',
+  Tertiary: '#f7f7f7'
+};
+
+const updatedSampleConfig = {
+  uploadEndpoint: 'https://httpbin.org/post',
+  multiple: true,
+  maxFiles: 5,
+  acceptedFileTypes: ['image/jpeg', 'image/png', 'application/pdf'],
+  maxSize: 5000000,
+  showPreview: true,
+  fileNameDisplay: true,
+  progressDisplay: true,
+  allowCancel: true,
+  allowDownload: true,
+  allowDelete: true,
+  styles: {
+    color: Colors.MainText,
+    progressColor: Colors.Primary,
+    backgroundColor: Colors.Tertiary,
+  },
+  dropzoneText: '...or drag and drop files here',
+  uploadButtonText: 'Upload a File',
+};
+
 export default function App() {
-  return <FileUploader />;
+  const onUploadComplete = () => {
+    
+  }
+  return <FileUploader config={updatedSampleConfig} />;
 }
--- src/FileUploader.js
+++ src/FileUploader.js
@@ -1,54 +1,126 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import styled from "styled-components";
 
-const FileUploader = () => {
-  const [files, setFiles] = useState([]);
+const DropzoneContainer = styled.div`
+  border: 2px dashed
+    ${(props) =>
+      props.isDragActive ? props.config.styles.progressColor : "#ccc"};
+  padding: 20px;
+  background-color: ${(props) => props.config.styles.backgroundColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  cursor: pointer;
+`;
+
+const UploadButton = styled.button`
+  background-color: ${(props) => props.config.styles.progressColor};
+  color: ${(props) => props.config.styles.color};
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${(props) => props.config.styles.backgroundColor};
+  }
+`;
+
+const FileItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const CheckmarkIcon = styled.span`
+  color: green;
+  font-size: 18px;
+  margin-right: 5px;
+`;
+
+const FileUploader = ({
+  config,
+  initialFiles = [],
+  onUploadStart,
+  onUploadProgress,
+  onUploadComplete,
+  onUploadError,
+  onUploadCancel,
+  onFilesChange,
+  editing = false,
+  error = "",
+  serverErrors = [],
+  serverWarnings = [],
+}) => {
+  const [files, setFiles] = useState(initialFiles);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
   const [message, setMessage] = useState("");
 
-  const handleFileChange = (event) => {
-    setFiles([...event.target.files]);
-    setMessage(""); // Reset the message when new files are selected.
+  const onDrop = (acceptedFiles) => {
+    setFiles([...files, ...acceptedFiles]);
+    if (onFilesChange) onFilesChange([...files, ...acceptedFiles]);
   };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: config.multiple,
+    maxFiles: config.maxFiles,
+    accept: config.acceptedFileTypes,
+    maxSize: config.maxSize,
+  });
 
   const handleUpload = async () => {
     if (files.length === 0) {
-      setMessage("Please select files to upload.");
       return;
     }
 
     setMessage("Uploading...");
 
-    // Prepare FormData to send files
     const formData = new FormData();
     files.forEach((file, index) => {
       formData.append(`file${index + 1}`, file);
     });
 
     try {
-      const response = await axios.post("https://httpbin.org/post", formData);
-
-      console.log(response.data); // Log the response for debugging purposes
-      setMessage("Files uploaded successfully!");
+      const response = await axios.post(config.uploadEndpoint, formData);
+      console.log(response.data);
+      setUploadedFiles([...uploadedFiles, ...files]);
+      setMessage("");
+      if (onUploadComplete) onUploadComplete(files, response.data);
     } catch (error) {
       setMessage("Error uploading files.");
       console.error("Upload error:", error);
+      if (onUploadError) onUploadError(files, error);
     }
   };
 
   return (
-    <div>
-      <input type="file" multiple onChange={handleFileChange} />
-      <div>
-        Selected Files:
-        <ul>
-          {files.map((file, index) => (
-            <li key={index}>{file.name}</li>
-          ))}
-        </ul>
-      </div>
-      <button onClick={handleUpload}>Upload</button>
+    <DropzoneContainer
+      {...getRootProps()}
+      isDragActive={isDragActive}
+      config={config}
+    >
+      <input {...getInputProps()} />
+      <UploadButton onClick={handleUpload} config={config}>
+        {config.uploadButtonText}
+      </UploadButton>
+      <p>{config.dropzoneText}</p>
+      <ul>
+        {files.map((file, index) => (
+          <FileItem key={index}>
+            {uploadedFiles.includes(file) && <CheckmarkIcon>✔</CheckmarkIcon>}
+            {file.name}
+          </FileItem>
+        ))}
+      </ul>
       <div>{message}</div>
-    </div>
+    </DropzoneContainer>
   );
 };
 
